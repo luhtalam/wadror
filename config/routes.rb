b3b54bb315731029649ec13c55e456af9c911e6c
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2017-01-28 17:22:17 +0200

    viikko 2 eteenpäin

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   get 'ratings', to: 'ratings#index'
 
+  get 'ratings/new', to: 'ratings#new'
+
+  post 'ratings', to: 'ratings#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
